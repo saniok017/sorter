6374219676402f6f44280ad8e20bddc5e779461d
saniok017 <saniok017@gmail.com>
2018-09-23 19:27:02 +0300

    sorter/3rdTask: without comparator

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr =[];
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
-
   at(index) {
-    // your implementation
+    return this.arr[index]
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+  return this.arr
   }
 
   sort(indices) {
-    // your implementation
+    this.indices = indices;
+    this.indices.sort();
+    this.temp = [];
+    this.tosort = [];
+    this.prefix = [];
+    // in 1 piece
+    if(this.arr.length == this.indices.length){
+      return this.arr.sort();}
+    // in 3 pieces
+    if(this.indices[0] !== 0){
+      this.prefix = this.arr.splice(0,this.indices[0]);
+      this.tosort = this.arr.slice(0,this.indices.length);
+      this.temp = this.arr.slice(this.indices.length);
+    // compare numbers with function
+      this.tosort.sort(function(a,b){
+        if(a>b){return 1} 
+        if(a<b){return -1}
+        });
+      return this.arr = this.prefix.concat(this.tosort,this.temp);
+    }
+    // in 2 pieces 
+    if (this.arr.length !== this.indices.length){
+      this.tosort = this.arr.slice(this.indices[0],this.indices.length);
+      this.temp = this.arr.slice(this.indices.length);
+      this.tosort.sort();
+      return this.arr = this.tosort.concat(this.temp);}
   }
 
   setComparator(compareFunction) {
