08896a829e3a7d2691f5ce94d9f94acce0c15a1d
saniok017 <saniok017@gmail.com>
2018-09-24 01:59:56 +0300

    sorter/3rdTask: new method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 class Sorter {
   constructor() {
     this.arr =[];
+    this.numbers = function(a,b){
+      return a-b;
+    }
+    this.comparator=this.numbers
   }
 
   add(element) {
@@ -26,29 +30,26 @@ class Sorter {
     this.prefix = [];
     // in 1 piece
     if(this.arr.length == this.indices.length){
-      return this.arr.sort();}
+      return this.arr.sort(this.comparator);}
     // in 3 pieces
     if(this.indices[0] !== 0){
       this.prefix = this.arr.splice(0,this.indices[0]);
       this.tosort = this.arr.slice(0,this.indices.length);
       this.temp = this.arr.slice(this.indices.length);
     // compare numbers with function
-      this.tosort.sort(function(a,b){
-        if(a>b){return 1} 
-        if(a<b){return -1}
-        });
+      this.tosort.sort(this.comparator);
       return this.arr = this.prefix.concat(this.tosort,this.temp);
     }
     // in 2 pieces 
     if (this.arr.length !== this.indices.length){
       this.tosort = this.arr.slice(this.indices[0],this.indices.length);
       this.temp = this.arr.slice(this.indices.length);
-      this.tosort.sort();
+      this.tosort.sort(this.comparator);
       return this.arr = this.tosort.concat(this.temp);}
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
